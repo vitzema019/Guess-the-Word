5b7d044adb089d01f7a0aaa6fcc1defae8376807
vitzema019 <71689674+vitzema019@users.noreply.github.com>
2024-01-05 06:57:06 +0100

    #2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Text.RegularExpressions;
-const int defaultHelth = 12; //defaultní hodnota životů (používa se při resetování hry)
-int helth = defaultHelth; //počet životů
-string[] words = new string[]
+using System.Text.RegularExpressions; //importování knihovny Regex
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+// Game: Guess the Word
+// Autor: Vít Zeman
+// Datum: 2024-01-5 (rrrr-mm-dd)
+// Verze: 1.0.0
+// Popis: Hra, ve které hráč hádá slova z oblasti informačních technologií
+// Programovací jazyk: C#
+// Programovací prostředí: Visual Studio Code
+
+// Changelog:
+// (rrrr-mm-dd) - (popis změny)
+// 2024-01-5 - vytvoření první verze programu programu
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Podel potřeby lze tyto proměnné změnit
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+const int defaultHealth = 12; //defaultní hodnota životů (používa se při resetování hry)
+const int textSpeed = 25; //rychlost výpisu textu (používa se při výpisu textu)
+const int borderTextSpeed = 5; //rychlost zobrazení ohraničení (používa se při výpisu textu)
+string[] words = new string[] //pole stringů, které obsahuje všechna slova, která mohou být hádána
 {
     "programming", "database", "software", "developer", "algorithm", "coding", "debugging", "frontend", "backend", "server",
     "network", "web", "security", "encryption", "authentication", "framework", "user", "interface", "function",
     "method", "variable", "object", "class", "API", "library", "code", "repository", "git", "branch",
-    "merge", "pull", "push", "commit", "bug", "feature", "release", "deployment", "version", "control", 
+    "merge", "pull", "push", "commit", "bug", "feature", "release", "deployment", "version", "control",
     "testing", "unit", "integration", "automation", "scripting", "codebase",
     "debugger", "console", "terminal", "server", "client", "protocol", "HTTP", "HTTPS", "CSS", "HTML",
     "JavaScript", "Python", "Java", "SQL", "MongoDB", "MySQL", "Firebase", "cloud", "container",
@@ -17,51 +36,74 @@ string[] words = new string[]
     "responsive", "design", "mobile", "application", "framework", "responsive", "deployment",
     "pipeline", "automation", "scripting", "script", "shell", "PowerShell", "lambda"
 };
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
+int health = defaultHealth; //počet životů
 var validCharacters = new Regex("^[a-z]$"); //nastavení validních znaků, které mohou být zadány
 bool win = false; //proměnná, která určuje, zda-li hráč vyhrál hru
 List<string> guessedLetters = new List<string>(); //list stringů, který obsahuje všechny již hádané znaky
-while (true)
+
+ConsoleSlowWriteLine("-----------------------------------------------------", borderTextSpeed);
+ConsoleSlowWriteLine("Vítejte v hře Guess the Word.");
+ConsoleSlowWriteLine("Cílem této hry je odhadnout slovo na základě jeho délky, které je znázorněno podtržítky.");
+ConsoleSlowWriteLine($"Máte k dispozici {health} životů, přičemž každý nesprávný pokus vás stojí jeden život.");
+ConsoleSlowWriteLine("Všechna slova, která budete hádat, jsou z oblasti informačních technologií a jsou v anglickém jazyce.");
+ConsoleSlowWriteLine("Přeji vám hodně štěstí!");
+ConsoleSlowWriteLine("-----------------------------------------------------", borderTextSpeed);
+ConsoleSlowWriteLine("Pro zahájení stiskněte klávesu ENTER.");
+var keyInf = Console.ReadKey(); //přečtení klávesy
+if (keyInf.Key == ConsoleKey.Escape) //pokud je stisknuta klávesa escape, hra se ukončí (přidáno z důvodu zláštního chování programu při stisknutí klávesy escape (loop))
 {
-    Console.WriteLine("Vítejte ve hře Hádej slovo");
-    Console.WriteLine("Vaším cílem je zijsit slovo podle jeho délky, které je vypsáno podtržítky");
-    Console.WriteLine($"Máte {defaultHelth} životů, za každé špatné hádání se vám počet životů sníží o 1");
-    Console.WriteLine("Všechna slova jsou z terminologie IT oblasti a jsou v anglickém jazyce");
-    Console.WriteLine("Hodně štěstí");
-    Console.WriteLine("Hra začíná");
-    Console.WriteLine(string.Empty);
+    Environment.Exit(0); //ukončení programu
+}
+Console.Clear();
+bool colorChange = false;
+
+while (true) //cyklus, který se opakuje dokud hráč neukončí hru
+{
+    colorChange = false;
     string selectedWord = SelectWord(words); //slovo, které je aktuálně hádáno
     string[] secret = Secret(selectedWord); //array stringů naplněný podtržítky, které budou reprezentovat neodhalené znaky slova (postupně se budou nahrazovat odhalenými znaky)
     string[] result = Split(selectedWord); //array stringů naplněný znak kompletně odhaleného slova
-    while (helth != 0 || helth > 0) //cyklus, který se opakuje dokud má hráč životy
+    bool firstRun = true; //proměnná, která určuje, zda-li je první spuštění hry
+    while (health != 0 || health > 0) //cyklus, který se opakuje dokud má hráč životy
     {
-        Console.WriteLine("---------------------------------");
-        Console.Write($"Počet životů: {helth}\n");
-        Console.Write("Hádané slovo: ");
+        if (firstRun)
+        {
+            //pouze pro vzhled
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(string.Empty); //vypsání prázdného řádku
+            }
+            firstRun = false;
+        }
+        Console.WriteLine("-----------------------------------------------------");
+        Console.Write($"Počet životů: {health}\n");
+        Console.Write("Aktuální slovo k odhadnutí: ");
         foreach (string item in secret) //vypsání aktuálního stavu hádaného slova
         {
             Console.Write($"{item} ");
         }
-        Console.WriteLine("\n---------------------------------");
+        Console.WriteLine("\n-----------------------------------------------------");
         Console.WriteLine(string.Empty);
         Console.Write("Zadejte písmeno: ");
         var keyInfo = Console.ReadKey(); //přečtení klávesy
-        if(keyInfo.Key == ConsoleKey.Escape) //pokud je stisknuta klávesa escape, hra se ukončí (přidáno z důvodu zláštního chování programu při stisknutí klávesy escape (loop))
+        if (keyInfo.Key == ConsoleKey.Escape) //pokud je stisknuta klávesa escape, hra se ukončí (přidáno z důvodu zláštního chování programu při stisknutí klávesy escape (loop))
         {
             Environment.Exit(0); //ukončení programu
         }
-        var key =keyInfo.Key.ToString().ToLower(); //převedení klávesy na string a převedení na malá písmena
+        var key = keyInfo.Key.ToString().ToLower(); //převedení klávesy na string a převedení na malá písmena
         Console.WriteLine(string.Empty);
+        Console.Clear();
         if (!validCharacters.IsMatch(key)) //pokud je klávesa nevalidní, program se vrátí na začátek cyklu
         {
-            Warning("Zdávejte pouze písmena abecedy");
+            Warning("Prosím, zadávejte pouze písmena abecedy.");
             continue; //pokračování na začátek cyklu
         }
         else if (guessedLetters.Contains(key)) //pokud je klávesa již hádaná, program se vrátí na začátek cyklu
         {
-            Warning("Toto písmeno jste již zkusili");
+            Warning($"Již jste zkoušeli písmeno |{key}|.");
             continue; //pokračování na začátek cyklu
         }
         else
@@ -70,7 +112,7 @@ while (true)
         }
         if (result.Contains(key)) //pokud je hádané písmeno v hádaném slově, program pokračuje dále
         {
-            Success("Toto písmeno se v tomto slově nachází !!!");
+            Success($"Písmeno |{key}| se v tomto slově nachází. Gratuluji");
             secret = Replace(result, key, secret); //nahrazení podtržítek odhalenými písmeny
             win = CheckWin(secret); //kontrola, zda-li je hádané slovo kompletně odhaleno
             if (win) //pokud je hádané slovo kompletně odhaleno, program ukončí cyklus
@@ -80,20 +122,16 @@ while (true)
         }
         else //pokud není hádané písmeno v hádaném slově, program sníží počet životů o 1
         {
-            Warning("Toto písmeno se v tomto slově nenachází, byl vám ubrán jeden život !!!");
-            helth--;
+            Warning($"Písmeno |{key}| se v tomto slově nenachází. Bohužel, byl vám odečten jeden život.");
+            health--;
         }
     }
 
     if (win) //pokud je proměnná win true, hráč vyhrál hru
     {
-        Success("Gratuluji, vyhrál jste");
-        Console.WriteLine($"Hádané slovo: {selectedWord}");
-        Console.WriteLine("Chcete hrát znovu? (y/n)");
-        var key = Console.ReadKey().Key.ToString().ToLower(); //přečtení klávesy a převedení na malá písmena a převedení na string
-        if (key == "y")
+        if (EndScreen("Gratuluji k vítězství ve hře! Úspěšně jste uhádl/a slovo.", selectedWord, win))
         {
-            ResetValues(); //resetování hodnot
+            Console.Clear();
             continue;
         }
         else
@@ -103,14 +141,10 @@ while (true)
     }
     else
     {
-        Warning("Bohužel jste prohrál");
-        Console.WriteLine($"Hádané slovo: {selectedWord}");
-        Console.WriteLine("Chcete hrát znovu? (y/n)");
-        var key = Console.ReadKey().Key.ToString().ToLower(); //přečtení klávesy a převedení na malá písmena a převedení na string
-        if (key == "y")
+        //pokud je proměnná win false, hráč prohrál hru
+        if (EndScreen("Je mi líto, že jste prohrál/a. Nezoufejte, klíčová je zábava a příležitost se něco nového naučit. :)", selectedWord, win))
         {
-            Console.WriteLine("\n");
-            ResetValues(); //resetování hodnot
+            Console.Clear();
             continue;
         }
         else
@@ -120,6 +154,67 @@ while (true)
     }
 }
 
+//Funkce a procedury
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// EndScreen() - vypíše zprávu, která je předána jako parametr, a nabídne hráči možnost hrát znovu
+bool EndScreen(string message, string selectedWord, bool win)
+{
+    string key = "";
+    do //cyklus, který se opakuje dokud hráč nezadá validní klávesu
+    {
+        Console.Clear(); //vyčištění konzole
+        if (win) //pokud je proměnná win true, hráč vyhrál hru
+        {
+            Success(message); //vypsání zprávy
+        }
+        else
+        {
+            Warning(message); //vypsání zprávy
+        }
+        Console.WriteLine($"Hádané slovo: {selectedWord}");
+        ConsoleSlowWriteLine("Chcete hrát znovu? (y/n)", 0);
+        var keyInfo = Console.ReadKey(); //přečtení klávesy
+        if (keyInfo.Key == ConsoleKey.Escape) //pokud je stisknuta klávesa escape, hra se ukončí (přidáno z důvodu zláštního chování programu při stisknutí klávesy escape (loop))
+        {
+            Environment.Exit(0); //ukončení programu
+        }
+        key = keyInfo.Key.ToString().ToLower(); //přečtení klávesy a převedení na malá písmena a převedení na string
+    } while (key != "y" && key != "n");
+
+
+    if (key == "y")
+    {
+        Console.WriteLine("\n");
+        ResetValues(); //resetování hodnot
+        //continue;
+        return true;
+    }
+    else
+    {
+        //break;
+        return false;
+    }
+}
+
+/*void ConsoleSlowWrite(string text, int delay = textSpeed) //funkce, která vypíše text po znacích s určitým zpožděním
+{
+    foreach (char c in text)
+    {
+        Console.Write(c);
+        System.Threading.Thread.Sleep(delay);
+    }
+}*/
+
+void ConsoleSlowWriteLine(string text, int delay = textSpeed) //funkce, která vypíše text po znacích s určitým zpožděním
+{
+    text += " \n";
+    foreach (char c in text)
+    {
+        Console.Write(c);
+        System.Threading.Thread.Sleep(delay);
+    }
+}
 
 // CheckWin() - vrací true, pokud je pole stringů, které je předáno jako parametr, kompletně odhaleno (neobsahuje podtržítka)
 static bool CheckWin(string[] arr)
@@ -150,7 +245,7 @@ static string[] Replace(string[] arr, string key, string[] result)
 
 // Secret() - vrací pole stringů, které obsahuje stejný počet prvků jako je počet znaků v textu, který je uložen v poli words. Každý prvek tohoto pole obsahuje znak podtržítka.
 static string[] Secret(string word)
-{ 
+{
     string[] textArr = new string[word.Length]; //vytvoření pole stringů o délce slova
     for (int i = 0; i < word.Length; i++) //procházení pole
     {
@@ -173,24 +268,33 @@ static string[] Split(string text)
 // SelectWord() - vrací náhodné slovo z pole words
 static string SelectWord(string[] words)
 {
-    Random rnd = new Random();
-    int index = rnd.Next(0, words.Length);
-    return words[index];
+    Random rnd = new Random(); //vytvoření instance třídy Random
+    int index = rnd.Next(0, words.Length); //vytvoření náhodného čísla
+    return words[index]; //vrácení náhodného slova
 }
 
 // ResetValues() - resetuje hodnoty proměnných
 void ResetValues()
 {
-    win = false;
-    helth = defaultHelth;
-    guessedLetters.Clear();
+    win = false; //resetování proměnné win
+    health = defaultHealth; //resetování počtu životů
+    guessedLetters.Clear(); //vyčištění listu
 }
 
 // Warning() - vypíše text v červené barvě
 void Warning(string text)
 {
     Console.WriteLine(string.Empty);
-    Console.ForegroundColor = ConsoleColor.Red;
+    if (colorChange == false) //pokud je colorChange false, vypíše text v červené barvě, pokud je colorChange true, vypíše text v tmavě červené barvě
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        colorChange = true; //změní hodnotu proměnné colorChange na true
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        colorChange = false; //změní hodnotu proměnné colorChange na false
+    }
     Console.WriteLine(text);
     Console.ResetColor();
     Console.WriteLine(string.Empty);
@@ -200,8 +304,18 @@ void Warning(string text)
 void Success(string text)
 {
     Console.WriteLine(string.Empty);
-    Console.ForegroundColor = ConsoleColor.Green;
+    if (colorChange == false)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        colorChange = true; //změní hodnotu proměnné colorChange na true
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        colorChange = false; //změní hodnotu proměnné colorChange na false
+    }
     Console.WriteLine(text);
     Console.ResetColor();
     Console.WriteLine(string.Empty);
 }
+//------------------------------------------------------------------------------------------------------------------------------------------------------
